c2bc57c32c964330685ebc709328b214d086cc19
Mr Foot <phuvo2105@gmail.com>
2022-10-28 23:51:22 +0700

    Update homework

--- 28-10-2022/index.js
+++ 28-10-2022/index.js
@@ -1,9 +1,24 @@
 const http = require('http')
 const fs = require('fs')
 
+// Read file sync style
+// const server = http.createServer((req, res) => {
+//     res.writeHead(200, {
+//         "Content-Type" : "text/html"
+//     })
+//     res.end(fs.readFileSync('./main.html', {encoding:'utf-8', flag:'r'}))
+// })
+
+// Read file async style
 const server = http.createServer((req, res) => {
     res.writeHead(200, {
         "Content-Type" : "text/html"
     })
-    res.end(fs.readFileSync('./main.html', {encoding:'utf-8', flag:'r'}))
+    fs.readFile('./main.html', {encoding: 'utf-8'}, (err, data) => {
+        res.end(data)
+    })
+})
+
+server.listen(9999, () => {
+    console.log('Serves is listening!!!')
 })
